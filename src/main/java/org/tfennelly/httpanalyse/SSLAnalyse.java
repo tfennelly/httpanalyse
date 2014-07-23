18917330428662c9a57dc4278003117dc75074aa
tfennelly <tom.fennelly@gmail.com>
2014-07-23 11:07:46 +0100

    certs

--- src/main/java/org/tfennelly/httpanalyse/SSLAnalyse.java
+++ src/main/java/org/tfennelly/httpanalyse/SSLAnalyse.java
@@ -6,6 +6,7 @@ import org.tfennelly.httpanalyse.common.HttpResponse;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.cert.Certificate;
 
 /**
  * SSL Analyse
@@ -18,5 +19,14 @@ public class SSLAnalyse {
 
         System.out.println(response.statusCode());
         System.out.println(response.contentAsString());
+
+        Certificate[] certs = response.getServerCerts();
+        if (certs != null && certs.length > 0) {
+            System.out.println(certs[0]);
+//            for (Certificate cert : certs) {
+//            }
+        } else {
+            System.out.println("No certs");
+        }
     }
 }
